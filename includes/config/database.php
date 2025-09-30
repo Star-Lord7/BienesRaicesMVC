4e3405f717168bb276deca60a93b8913360e24f8
Star-Lord7 <samuelnasa375@gmail.com>
2025-09-30 11:03:30 -0600

    Configuracion de Variables de Entorno para Railway

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,27 +1,44 @@
 <?php
 
-require_once __DIR__ . '../../../vendor/autoload.php';
-
-use Dotenv\Dotenv; //libreria para cargar variables de entorno
-
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
-
 function conectarDB() : mysqli {
-    $host = $_ENV['MYSQL_ADDON_HOST']; // MYSQL_ADDON_HOST
-    $user = $_ENV['MYSQL_ADDON_USER']; // MYSQL_ADDON_USER
-    $pass = $_ENV['MYSQL_ADDON_PASSWORD']; // MYSQL_ADDON_PASSWORD
-    $db   = $_ENV['MYSQL_ADDON_DB']; // MYSQL_ADDON_DB
-    $port = $_ENV['MYSQL_ADDON_PORT']; // MYSQL_ADDON_PORT
+    // Leer las variables de entorno directamente
+    $host = getenv('MYSQL_ADDON_HOST');
+    $user = getenv('MYSQL_ADDON_USER');
+    $pass = getenv('MYSQL_ADDON_PASSWORD');
+    $db   = getenv('MYSQL_ADDON_DB');
+    $port = getenv('MYSQL_ADDON_PORT');
 
-    $db = mysqli_connect($host, $user, $pass, $db, $port);
+    $conexion = mysqli_connect($host, $user, $pass, $db, $port);
 
-    if(!$db){
-        echo 'Error: No se pudo conectar a la base de datos -> ' . mysqli_connect_error();
-        exit;
+    if (!$conexion) {
+        die('Error: No se pudo conectar a la base de datos -> ' . mysqli_connect_error());
     }
-    return $db;
+
+    return $conexion;
 }
+
+// require_once __DIR__ . '../../../vendor/autoload.php';
+
+// use Dotenv\Dotenv; //libreria para cargar variables de entorno
+
+// $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+// $dotenv->load();
+
+// function conectarDB() : mysqli {
+//     $host = $_ENV['MYSQL_ADDON_HOST']; // MYSQL_ADDON_HOST
+//     $user = $_ENV['MYSQL_ADDON_USER']; // MYSQL_ADDON_USER
+//     $pass = $_ENV['MYSQL_ADDON_PASSWORD']; // MYSQL_ADDON_PASSWORD
+//     $db   = $_ENV['MYSQL_ADDON_DB']; // MYSQL_ADDON_DB
+//     $port = $_ENV['MYSQL_ADDON_PORT']; // MYSQL_ADDON_PORT
+
+//     $db = mysqli_connect($host, $user, $pass, $db, $port);
+
+//     if(!$db){
+//         echo 'Error: No se pudo conectar a la base de datos -> ' . mysqli_connect_error();
+//         exit;
+//     }
+//     return $db;
+// }
 
 // function conectarDB() : mysqli {
 //     $db = new mysqli('localhost', 'root', '', 'bienesraices_crud');
